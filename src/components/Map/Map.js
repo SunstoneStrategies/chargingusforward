4c0e64b4f2f85f39d6736f202e4d685700ea6e5c
Zubi1001 <zubair1234566@gmail.com>
2023-05-29 17:53:42 +0500

    marker changed

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -88,13 +88,13 @@ export default function Map() {
                   <circle cx="12" cy="10" r="2" fill="white" />
                   <path
                     d="M12 24.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 14.7z"
-                    fill="yellow"
+                    fill="#fcbc0c"
                     stroke="white"
-                    strokeWidth="3"
+                    strokeWidth="1"
                     strokeLinecap="round"
                     strokeLinejoin="round"
                   />
-                  <circle cx="12" cy="10" r="4" fill="yellow" />
+                  <circle cx="12" cy="10" r="4" fill="#fcbc0c" />
                   <circle cx="12" cy="10" r="2" fill="white" />
                 </g>
               </Marker>
